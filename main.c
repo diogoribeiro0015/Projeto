6179de870cdc9781c4dbe74e58e54b64bc8f6097
diogoribeiro0015 <44406304+diogoribeiro0015@users.noreply.github.com>
2018-10-29 16:47:20 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /******************************************************************************
 * File Name: main.c
-* Authors: Diogo Ribeiro nº
-*          Ricardo Caetano nº
+* Authors: Diogo Ribeiro nº 90051
+*          Ricardo Caetano nº 90177
 * Last modified: /11/2108
 * COMMENTS
 *		Main program
@@ -20,15 +20,33 @@
 * Returns: int
 * Description: main Program
 *****************************************************************************/
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
 
   FILE *fp;
   Mapa *Head;
+  int x_inicio[1000][8]={{0}}, y_inicio[1000][8]={{0}};
+  int x_fim[1000][8]={{0}}, y_fim[1000][8]={{0}};
+  int i=0, a=0, x=0, y=0;
 
   VerifyCommandLine(argc, argv);
   fp = OpenFile(argv);
   Head = ReadFile(fp);
 
+  x=Head->coord->xcoord;
+  y=Head->coord->ycoord;
 
+  while (x_inicio!=x_inicio && y_fim!=y_inicio)
+  {
+    RandomPlay(x,y,x_inicio, y_inicio);
+    for(i=0; i<8; i++)
+    {
+      for(a=0; a<8; a++)
+      {
+        //if(x_inicio[x][i]==x_fim[x_final][a] && y_hipotese[y_inicial][i]==y_hipotese[y_final][a])
+      }
+    }
+
+  }
   return(0);
 }
